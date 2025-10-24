79dc96a891b221dfffa801282200a8ff5a1a5d04
ajinformatica130-png <ajinformatica130@gmail.com>
2025-10-23 23:26:58 -0300

    Refactor MainActivity class structure

--- android/app/src/main/kotlin/com/tochegando/motoboy/MainActivity.kt
+++ android/app/src/main/kotlin/com/tochegando/motoboy/MainActivity.kt
@@ -4,9 +4,3 @@ import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity : FlutterFragmentActivity() {
 }
-package com.tochegando.motoboy
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity() {
-}
